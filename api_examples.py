7f71d62605ad00e77b426a4cd85b389f78fc43e5
John Kranz <johnk@spectralogic.com>
2022-10-17 09:11:09 -0600

    Fix scan with options -- add get jobs status (#5)
    * Fix scan with options
    
    * Add jobs status example
    
    Co-authored-by: John Kranz <johnk@sldomain.com>

--- api_examples.py
+++ api_examples.py
@@ -50,6 +50,7 @@ example_source_share_optional = os.path.join(example_directory_root, "example-so
 example_source_share = os.path.join(example_directory_root, "example-source-storage-location")
 example_target_share_optional = os.path.join(example_directory_root, "example-target-storage-location-optional")
 example_target_share = os.path.join(example_directory_root, "example-target-storage-location")
+example_scan_working_directory = os.path.join(os.sep, "WorkingDirectory")
 
 example_include_directory = "IncludeDirectory"
 example_exclude_directory = "ExcludeDirectory"
@@ -219,19 +220,13 @@ def make_instance_scan_project(include_optional):
         return ApiProjectScan(
             description='A description of the scan project',
             share='Example-Source-Optional-Fields',
-            working_directory=os.path.join(os.path.abspath(os.sep), 'WorkingDirectory'),
+            working_directory=example_scan_working_directory,
             tags=[
                 'tag'
             ],
-            filter={"excludeDirectories": [example_exclude_directory],
-                    "minimumAge": "AnyAge", "includeDirectories": [example_include_directory]},
             schedule={"monthsOfYear": ["monthsOfYear", "monthsOfYear"], "period": "Now", "daysOfMonth": [0, 0],
                       "startMonth": 3, "startDay": 5, "startHour": 13, "startYear": 2021, "interval": 6,
                       "daysOfWeek": ["daysOfWeek", "daysOfWeek"], "startMinute": 33},
-            status={"name": "Example-Scan-Name", "nextRunTime": "2021-01-23T04:56:07.000Z",
-                    "createdBy": "Example User", "created": "2020-01-23T04:56:07.000Z",
-                    "lastSuccessfulJob": "2020-01-23T04:56:07.000Z", "lastJobRunTime": "2020-01-23T04:56:07.000Z",
-                    "updated": "2020-01-23T04:56:07.000Z", "conflictDetected": "2020-01-23T04:56:07.000Z"}
         )
     # Example create/update scan project request body with required fields
     else:
@@ -522,6 +517,34 @@ def example_api_get_project():
         except ApiException as e:
             print("Exception when calling ProjectApi->list_projects (example GET list of projects): %s\n" % e)
 
+def example_api_get_jobs():
+    #  Set the configuration
+    configuration = openapi_client.Configuration(
+        host=example_host,
+    )
+    configuration.verify_ssl = False
+    configuration.ssl_ca_cert = None
+    configuration.assert_hostname = False
+    configuration.cert_file = None
+
+    #  Get an access token and add it to the configuration
+    with openapi_client.ApiClient(configuration) as api_client:
+        api_instance_auth = openapi_client.AuthenticationApi(api_client)
+        body_login = openapi_client.ApiCredentials(password=example_password, username=example_username)
+        api_response = api_instance_auth.login(body_login)
+        configuration.access_token = api_response.token
+
+        # Create a JobStatusAPI instance to make API commands
+        api_instance = openapi_client.JobStatusApi(api_client)
+
+        print("# Get five Job statuses")
+        try:
+            params = {"include_all": True, "limit": 5}
+            api_response = api_instance.list_all_jobs_status(**params)
+            pprint(api_response)
+        except ApiException as e:
+            print("Exception when calling JobStatusApi->list_all_jobs_status: %s\n" % e)
+
 # Runs each API call sequentially as to show workflow.
 def main():
     example_api_login()
@@ -533,6 +556,7 @@ def main():
     example_api_create_update_project_restore()
     example_api_get_storage_location()
     example_api_get_project()
+    example_api_get_jobs()
 
 if __name__ == '__main__':
     main()
